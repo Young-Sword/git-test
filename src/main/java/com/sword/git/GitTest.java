9bbba79a659d7c34a465dcd12be17a5b3a8d8f24
young <13559163528@139.com>
2025-01-04 21:38:34 +0800

    d1 commit

--- src/main/java/com/sword/git/GitTest.java
+++ src/main/java/com/sword/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello m1");
         System.out.println("hello m2");
+        System.out.println("hello d1");
     }
 }
